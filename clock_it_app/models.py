cc9a38dfd834696871cd4229b14130810b0f35a7
Dilorom Khakimova <dilya_khakimova@yahoo.com>
2020-08-28 13:28:01 -0700

    minor spacing fix

--- clock_it_app/models.py
+++ clock_it_app/models.py
@@ -8,7 +8,6 @@ class Timesheet(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-  
 
 class CompanyManager(models.Manager):
   
